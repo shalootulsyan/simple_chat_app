2044efb1993170fac404996512a81627afc12483
Shaloo Tulsyan <tulsyanshaloo@gmail.com>
2019-10-26 13:06:58 +0530

    fixed bug on login 1

--- src/screens/chatScreen.js
+++ src/screens/chatScreen.js
@@ -42,7 +42,7 @@ class ChatScreen extends React.Component {
         //console.log(value);
 
         await this.setState({
-            messageData: await Storage.getMessage()
+            messageData: await Storage.getMessage() || 'Hi'
         })
         //console.log(this.state.messageData[this.state.userName].message);
 
